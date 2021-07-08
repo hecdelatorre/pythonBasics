f980d34cf0e99f507a42befef47363c292f164e1
Hec98 <hector982015@gmail.com>
2021-07-08 18:24:39 -0500

    Add init/formatText.py

---
+++ init/formatText.py
@@ -0,0 +1,14 @@
+name = "Hari"
+age = 23
+print("Hello it's. %s" %name)
+print("%s is %d years old."%(name, age))
+
+x, y = 10, 33
+print('The value of x is {} and y is {}'.format(x, y))
+
+like = 'python'
+print('I like {}'.format(like))
+
+info = input('Enter your name: ').strip()
+print('Your name is {}'.format(info))
+
